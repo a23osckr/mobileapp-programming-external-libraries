f3945ba5e26ede0bbfe95f5814dbecdd4ed8ecec
AOkay12 <oscarkruit@live.se>
2024-05-08 12:00:18 +0200

    Added gradient to the dashed view.

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.externallibraries;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.mackhartley.dashedview.DashColorGenerator;
+import com.mackhartley.dashedview.DashedView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DashedView dash1 = findViewById(R.id.dashed);
+
+        dash1.setDashColorGenerator(new DashColorGenerator() {
+            @Override
+            public int getPaintColor(int curIndex, int numDashes) {
+                float alphaValue = 255 * ((curIndex + 1) / (float) (numDashes + 1));
+                int gradientAppliedColor = Color.argb((int) alphaValue, 255, 0, 255);
+                return gradientAppliedColor;
+            }
+        });
     }
 }
